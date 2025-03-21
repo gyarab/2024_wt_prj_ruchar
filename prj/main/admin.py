1bd9addedf4ff61f89099fa0dc8a76b576f0f4a7
Krystof Ruchar <krystof.ruchar.s@gyarab.cz>
2025-03-21 10:31:50 +0100

    hodina 21.3.

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Painting, Author
+
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year", "author", "description"]
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year", "description"]
 
 # Register your models here.
+admin.site.register(Painting, PaintingAdmin)
+admin.site.register(Author, AuthorAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Painting(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null= True)
+    description = models.TextField(blank=True, default="")
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Movie <{self.id}> {self.name}"
+    
+class Author(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null= True)
+    birth_year = models.IntegerField(blank=True, null= True)
+    description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"Author <{self.id}> {self.name}"

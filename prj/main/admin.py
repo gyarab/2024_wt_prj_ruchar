180adeb52483f0bdefe9d0cf02b7eacaf75144e5
Krystof Ruchar <krystof.ruchar.s@gyarab.cz>
2025-03-28 10:22:07 +0100

    hodina 28.3.

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
-from .models import Painting, Author
+from .models import Painting, Author, OriginNation, ArtMovement, Technique, Nationality
 
 class PaintingAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "year", "author", "description"]
+    list_display = ["id", "name", "year", "author", "origin_nation", "art_movement", "technique", "description"]
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "year", "description"]
+    list_display = ["id", "name", "birth_year", "death_year", "nationality", "profession", "description"]
+
+class OriginNationAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class ArtMovementAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class TechniqueAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class NationalityAdmin(admin.ModelAdmin):
+    list_display = ["name"]
 
 # Register your models here.
 admin.site.register(Painting, PaintingAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(OriginNation, OriginNationAdmin)
+admin.site.register(ArtMovement, ArtMovementAdmin)
+admin.site.register(Technique, TechniqueAdmin)
+admin.site.register(Nationality, NationalityAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -7,15 +7,44 @@ class Painting(models.Model):
     year = models.IntegerField(blank=True, null= True)
     description = models.TextField(blank=True, default="")
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    origin_nation = models.ForeignKey("OriginNation", on_delete=models.SET_NULL, null=True)
+    art_movement = models.ForeignKey("ArtMovement", on_delete=models.SET_NULL, null=True)
+    technique = models.ForeignKey("Technique", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"Movie <{self.id}> {self.name}"
+        return f"Painting <{self.id}> {self.name}"
     
 class Author(models.Model):
     name = models.CharField(max_length=300)
-    year = models.IntegerField(blank=True, null= True)
     birth_year = models.IntegerField(blank=True, null= True)
+    death_year = models.IntegerField(blank=True, null= True)
+    nationality = models.ForeignKey("Nationality", on_delete=models.SET_NULL, null=True)
+    profession = models.CharField(max_length=300, default="")
     description = models.TextField(blank=True, default="")
 
     def __str__(self):
         return f"Author <{self.id}> {self.name}"
+    
+class OriginNation (models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Nation <{self.id}> {self.name}"
+    
+class ArtMovement (models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Art Movement <{self.id}> {self.name}"
+    
+class Technique (models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Techique <{self.id}> {self.name}"
+    
+class Nationality (models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Nationality <{self.id}> {self.name}"

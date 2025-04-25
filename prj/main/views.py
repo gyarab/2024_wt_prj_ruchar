ee5653466af92cee2a0d9dd40b7eadf62672a12c
Krystof Ruchar <krystof.ruchar.s@gyarab.cz>
2025-04-25 10:29:42 +0200

    konec hodiny 25.4.

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Painting
+
 # Create your views here.
+
+def get_homepage(request):
+    context = {
+        "svatek": "Marek",
+        "paintings": Painting.objects.all().order_by('name') [:10]
+    }
+    return render(
+        request, "main/homepage.html", context
+        )

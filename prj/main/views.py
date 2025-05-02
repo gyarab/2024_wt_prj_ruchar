95696824984425be0622baffb6c2a357aec64e36
Krystof Ruchar <krystof.ruchar.s@gyarab.cz>
2025-05-02 10:20:57 +0200

    hodina 2.5.

--- prj/main/views.py
+++ prj/main/views.py
@@ -5,10 +5,20 @@ from .models import Painting
 # Create your views here.
 
 def get_homepage(request):
+    paintings = Painting.objects.all().order_by('name')
+    
+    if request.GET.get("search"):
+        print("SEARCH", request.GET.get("search"))
+        paintings = paintings.filter(name__contains=request.GET.get("search"))
+
     context = {
         "svatek": "Marek",
-        "paintings": Painting.objects.all().order_by('name') [:10]
+        "paintings": paintings
     }
+
+    # print("HOST", request.get_host())
+    # print("URL", request.path)
+
     return render(
         request, "main/homepage.html", context
         )

17be14242f4b20198b7d653f97b58622c579060a
Krystof Ruchar <krystof.ruchar.s@gyarab.cz>
2025-05-16 10:09:40 +0200

    první polovina 16.5.

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
 
-from .models import Painting
+from .models import Painting, ArtMovement
 
 # Create your views here.
 
 def get_homepage(request):
     paintings = Painting.objects.all().order_by('name')
     
-    if request.GET.get("search"):
-        print("SEARCH", request.GET.get("search"))
-        paintings = paintings.filter(name__contains=request.GET.get("search"))
+    search = request.GET.get('search')
+    if search:
+        paintings = paintings.filter(name__icontains=search) | paintings.filter(description__icontains=search)
+
+    art_movement = request.GET.get('artmovement')
+    if art_movement:
+        paintings = paintings.filter(art_movement__name=art_movement)
 
     context = {
         "svatek": "Marek",
-        "paintings": paintings
+        "paintings": paintings,
+        "art_movements": ArtMovement.objects.all().order_by('name'),
     }
-
-    # print("HOST", request.get_host())
-    # print("URL", request.path)
 
     return render(
         request, "main/homepage.html", context
         )
+
+def get_painting(request, id):
+    painting = Painting.objects.get(id=id)
+    context = {
+        "painting": painting,
+    }
+    return render(request, "main/painting.html", context)
